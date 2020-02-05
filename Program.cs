7f2fc7b62aadea83fb360d51915854dfad0414cd
Oleksandr Parshakov <stail.7ff@gmail.com>
2020-02-05 22:23:08 +0200

    Added Descending sorting project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Services;
 
 namespace Exercises
 {
@@ -12,11 +8,11 @@ namespace Exercises
         {
             Console.WriteLine("Enter numbers split by a comma:");
             var input = Console.ReadLine();
-            var array = input.Split(',');
+            var split = input.Split(',');
             var number = 0;
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < split.Length; i++)
             {
-                var temp = Convert.ToInt32(array[i]);
+                var temp = Convert.ToInt32(split[i]);
                 if (temp > number)
                 {
                     number = temp;
---
+++ Sorter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers split by a comma:");
+            try
+            {
+                var input = Console.ReadLine();
+                var split = input.Split(',');
+                var length = split.Length;
+                var inputInt = new int[length];
+                for (int i = 0; i < length; i++)
+                {
+                    inputInt[i] = Convert.ToInt32(split[i]);
+                }
+                var count = 1;
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = count; j < length; j++)
+                    {
+                        if (inputInt[i] < inputInt[j])
+                        {
+                            var x = inputInt[i];
+                            inputInt[i] = inputInt[j];
+                            inputInt[j] = x;
+                        }
+                    }
+                    count++;
+                }
+                Console.Write("Sorted numbers: ");
+                foreach (var item in inputInt)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Incorrect input");
+            }
+        }
+    }
+}
