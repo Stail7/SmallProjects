c861fabb085cfcc6cdcc5ab60a19bd65a21fb161
Oleksandr Parshakov <stail.7ff@gmail.com>
2020-02-06 00:17:56 +0200

    Added demo of Arrays and Lists

---
+++ DemoArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DemoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] {3, 7, 9, 2, 14, 6};
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf() - searches for index of an element (9 in example)
+            Console.WriteLine("Index of 9: " + Array.IndexOf(numbers, 9));
+
+            // Clear() - replaces a range of elements from starting index by default type values
+            Array.Clear(numbers, 0, 3);
+
+            Console.Write("Effect of Clear(): ");
+            foreach (var i in numbers)
+                Console.Write(i + " "); Console.WriteLine();
+
+            // Copy
+            var copyArr = new int[5];
+            Array.Copy(numbers, copyArr, 5 );
+
+            Console.Write("Effect of Copy(): ");
+            foreach (var i in numbers)
+                Console.Write(i + " "); Console.WriteLine();
+
+            // Sort()
+            Array.Sort(numbers);
+
+            Console.Write("Effect of Sort(): ");
+            foreach (var i in numbers)
+                Console.Write(i + " "); Console.WriteLine();
+
+            // Reverse()
+            Array.Reverse(numbers);
+
+            Console.Write("Effect of Reverse(): ");
+            foreach (var i in numbers)
+                Console.Write(i + " "); Console.WriteLine();
+        }
+    }
+}
---
+++ DemoLists/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace DemoLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() {1, 2, 3, 4};
+
+            numbers.Add(1);
+
+            numbers.AddRange(new int[]{ 5, 6, 7 });
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            // IndexOf 1
+            Console.WriteLine("\nIndexOf 1: " + numbers.IndexOf(1));
+            Console.WriteLine("LastIndexOf 1: " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+            // Remove 1
+            Console.Write("Remove 1: ");
+            numbers.Remove(1);
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            } Console.WriteLine();
+
+            // Remove all instances of 1
+            Console.Write("Remove all instances of 1: ");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            // Clear
+            numbers.Clear();
+            Console.WriteLine("\nCount after Clear(): " + numbers.Count);
+        }
+    }
+}
