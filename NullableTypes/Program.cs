76e61886761ad606d1924f3af53f6af4d7909dbe
Oleksandr Parshakov <stail.7ff@gmail.com>
2020-02-24 14:32:19 -0800

    Added Nullable Types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<DateTime> date = null;
+            // OR
+            DateTime? date2 = null;
+
+            Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault());
+            Console.WriteLine("HasValue: " + date.HasValue);
+            //Console.WriteLine("Value: " + date.Value); // extension if the value is null
+
+            DateTime? date3 = new DateTime(2015, 1, 1);
+            DateTime date4 = date3.GetValueOrDefault();
+            Console.WriteLine(date4);
+
+            // ?? Null Coalescing operator
+            
+            DateTime date5;
+
+            if (date2 != null)
+                date5 = date.GetValueOrDefault();
+            else
+                date5 = DateTime.Today;
+            // OR
+            date5 = date != null ? date.GetValueOrDefault() : DateTime.Today;
+            // OR
+            date5 = date ?? DateTime.Today; // if 'date' has a value than assign that value to 'date5'
+                                            // if 'date == null' assign DateTime.Today to 'date5'
+        }
+    }
+}
