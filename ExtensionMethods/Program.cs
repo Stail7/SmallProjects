2f1d1f4349e410259ee5888119a7ace66777f4f3
Oleksandr Parshakov <stail.7ff@gmail.com>
2020-02-24 12:27:05 -0800

    Added Extension Methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+            Console.WriteLine(shortenedPost);
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods 
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if(numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater then or equal to '0");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            //var newString = new StringBuilder();
+            //for (int i = 0; i < numberOfWords; i++)
+            //{
+            //    newString.Append(words[i] + " ");
+            //}
+            //return Convert.ToString(newString);
+
+            //// OR
+
+            // Join and Take are extension methods of the String class
+            return string.Join(" ", words.Take(numberOfWords) + "...");
+        }
+    }
+}
